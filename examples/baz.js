0e3d9619b6992e625a434cb052764d60237ce4fc
Andrey Sidorov <andrey.sidorov@digital-logic.com.au>
2015-11-27 16:25:12 +1100

    Initial support for pending breakpoints. Closes #30

--- examples/baz.js
+++ examples/baz.js
@@ -1,4 +1,17 @@
 module.exports = function() {
+
+  console.log(1);
+  console.log(2);
+  console.log(3);
+  console.log(4);
+  console.log(5);
+  console.log(6);
+  console.log(7);
+  console.log(8);
+  console.log(9);
+  console.log(10);
+
+
   console.log('baz 1');
   debugger;
   console.log('baz 2');
